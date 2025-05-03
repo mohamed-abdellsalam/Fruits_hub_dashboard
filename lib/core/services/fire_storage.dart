593410db8f29e0d4df74822e3ebdd545e9cd90d9
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-05-03 20:25:43 +0300

    Refactor FireStorage service to implement file upload functionality and update pubspec.yaml to include path dependency

--- lib/core/services/fire_storage.dart
+++ lib/core/services/fire_storage.dart
@@ -1,11 +1,18 @@
+import 'dart:io';
+
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:fruits_hub_dashboard/core/services/storage_services.dart';
+import 'package:path/path.dart' as b;
 
 class FireStorage implements StorageServices {
-  final storageRef = FirebaseStorage.instance;
+  final storageRef = FirebaseStorage.instance.ref();
   @override
-  Future<String> uploadImage(String imagePath) {
-    // TODO: implement uploadImage
-    throw UnimplementedError();
+  Future<String> uploadFile(File file, String path) async {
+    String fileName = b.basename(path);
+    String extensionName = b.extension(file.path);
+    var fileRef = storageRef.child('$path/$fileName.$extensionName');
+    await fileRef.putFile(file);
+    var fileUrl = await fileRef.getDownloadURL();
+    return fileUrl;
   }
 }
--- lib/core/services/storage_services.dart
+++ lib/core/services/storage_services.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 abstract class StorageServices {
-  Future<String> uploadImage(String imagePath);
+  Future<String> uploadFile(File file, String path);
 }
