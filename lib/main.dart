aaf546e768133c807999483e08c62224cb52d709
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-05-08 16:31:08 +0300

    Integrate Supabase and URL launcher plugins; update generated plugin registrants and dependencies across platforms

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:fruits_hub_dashboard/core/services/custom_bloc_observer.dart';
 import 'package:fruits_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruits_hub_dashboard/features/dashboard/views/dashboard_view.dart';
 import 'package:fruits_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,7 +14,11 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+  await Supabase.initialize(
+    url: 'https://tlipjdfcqhwaxemhnkgb.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsaXBqZGZjcWh3YXhlbWhua2diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MDk5NjYsImV4cCI6MjA2MjI4NTk2Nn0.AGBywCjvQm3XZKU76L_Vg3WaqVpWwMh2Ir344T0TI-c',
+  );
   setupGetit();
   runApp(const MainApp());
 }
